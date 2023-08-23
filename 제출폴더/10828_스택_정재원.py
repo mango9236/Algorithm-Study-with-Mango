fe2d7547e7e1f3ef1671e2c3434a46d20413d6b1
정재원 <104828805+gpffhwodnjs@users.noreply.github.com>
2023-08-24 00:06:24 +0900

    Update 10828_스택_정재원.py

--- 제출폴더/10828_스택_정재원.py
+++ 제출폴더/10828_스택_정재원.py
@@ -1,4 +1,12 @@
-N = int(input())
+import sys
+N = int(sys.stdin.readline())
+
+#그냥 input으로 하면 시간 초과
+#왜? 
+#input()은 sys.stdin.readline()보다 느리다.
+#input()은 동적으로 할당되는 메모리를 사용한다.
+#sys.stdin.readline()은 정적으로 할당되는 메모리를 사용한다.
+#따라서 input()은 시간이 더 걸린다. - 시간초과
 stack = []
 
 ###########이렇게 하니까 숫자를 입력하지 않는 커맨드가 들어오면 에러가 난다.
@@ -26,11 +34,12 @@ stack = []
 #             print(-1)
 #         else:
 #             print(lis[len(lis)-1])
-for _ in range(N):
-    command = input().split()  #리스트로 입력되기때문에 인덱스를 사용할 수 있음
+
+
+for _ in range(N):             
+    command = sys.stdin.readline().split()  #처음 생각과 다른 점-리스트로 입력되기때문에 인덱스를 사용할 수 있음
     if command[0] == 'push':
         stack.append(command[1])
-        print(*stack)
     elif command[0] == 'pop':
         if len(stack) == 0:
             print(-1)
