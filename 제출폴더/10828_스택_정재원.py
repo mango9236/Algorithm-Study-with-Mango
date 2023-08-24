641671ea433a6fdc9b2f1c921b22bb76599cd8dc
mingyu choi <52327569+mango9236@users.noreply.github.com>
2023-08-24 21:21:31 +0900

    Update 10828_스택_정재원.py

--- 제출폴더/10828_스택_정재원.py
+++ 제출폴더/10828_스택_정재원.py
@@ -37,7 +37,7 @@ stack = []
 
 
 for _ in range(N):             
-    command = sys.stdin.readline().split()  #처음 생각과 다른 점-리스트로 입력되기때문에 인덱스를 사용할 수 있음
+    command = sys.stdin.readline().split()  #처음 생각과 다른 점 - 리스트로 입력되기때문에 인덱스를 사용할 수 있음
     if command[0] == 'push':
         stack.append(command[1])
     elif command[0] == 'pop':
@@ -57,4 +57,9 @@ for _ in range(N):
             print(-1)
         else:
             print(stack[len(stack)-1])
+
+## 피드백
+좋은 공부였다고 생각합니다. 지금은 상관없지만, 나중에는 거의 대부분 sys.stdin.readline을 사용해야하므로 습관을 들여놓으면 좋습니다.
+나도 input받을때, (command, num) 이런 식으로 처음에 받았는데, 찍어가 보면서 바꾸는건 당연한겁니다.
+split하면 그 자체로 list로 던져주기때문에 그대로 써도될거같고, 
             
